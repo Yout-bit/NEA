7b13e28a123d1183c3830260ae0111baffdb310d
Yout-bit <75207892+Yout-bit@users.noreply.github.com>
2021-07-06 10:43:30 +0100

    Update grid.py

--- grid.py
+++ grid.py
@@ -2,29 +2,31 @@ import pygame
 import math
 
 
-
-def initailise_map(map):
-    row = []
-    fmap = []
-    Counter = 0
-    for charecter in map:
-        Counter += 1
-        row.append(charecter)
-        if Counter == 9:
-            fmap.append(row)
-            row = []
-            Counter = 0
-    return fmap
-
-
 class Grid():
-    def __init__(self, map ,tile_width):
+    def __init__(self, map, tile_width):
         self.tile_width = tile_width
-        self.map = initailise_map(map)
+        self.map = self.initailise_map(map)
 
     def get_grid_value(self, xy):
         x , y = xy
         return self.map[math.floor(x / self.tile_width)][math.floor(y / self.tile_width)]
+
+    def initailise_map(self, map):
+        row = []
+        fmap = []
+        Counter = 0
+        #print (self.tile_width)
+        for charecter in map:
+            #print(charecter, Counter)
+            Counter += 1
+            row.append(charecter)
+            if Counter == (900 / self.tile_width):
+                #print("new row")
+                fmap.append(row)
+                row = []
+                Counter = 0
+        print(fmap)
+        return fmap
     
     def draw(self, surface):
         for row in range(len(self.map)):
