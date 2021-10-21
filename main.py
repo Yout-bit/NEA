77edf21024846b2325d8f6d3c38c39c9e036a8af
Yout-bit <75207892+Yout-bit@users.noreply.github.com>
2021-10-21 13:10:35 +0100

    Spooky mode

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ from projectile import Projectile
 
 
 
+from datetime import date
+
+today = date.today()
+if today.month == 10 and today.day > 25:
+    Halloween = True
+else:
+    Halloween = False
+
+
 #Establishing coolours
 Colours = {
     "BLUEY" : (144, 137, 218),
@@ -74,6 +83,37 @@ def check_hit(player, projectile):
         else:
             return False
 
+
+def draw_menu(P1text, P2text, winner, P1Score, P2Score, COUNTDOWN):
+    DISPLAYSURF.blit(Text(112).render(P1text, True, Colours["SHADOW"]), (43, 123))
+    DISPLAYSURF.blit(Text(112).render(P2text, True, Colours["SHADOW"]), (43, 203))
+    DISPLAYSURF.blit(Text(112).render(winner, True, Colours["SHADOW"]), (43, 43))
+    DISPLAYSURF.blit(Text(112).render(P1text, True, Colours["BLACK"]), (40, 120))
+    DISPLAYSURF.blit(Text(112).render(P2text, True, Colours["BLACK"]), (40, 200))
+    DISPLAYSURF.blit(Text(112).render(winner, True, Colours["BLACK"]), (40, 40))
+
+    DISPLAYSURF.blit(Text(40).render("Scores:", True, Colours["SHADOW"]), (42, 414)) 
+    DISPLAYSURF.blit(Text(40).render("Scores:", True, Colours["BLACK"]), (40, 412)) 
+    DISPLAYSURF.blit(Text(40).render("P1", True, Colours["SHADOW"]), (47, 452))       
+    DISPLAYSURF.blit(Text(40).render("P2", True, Colours["SHADOW"]), (88, 452))
+    DISPLAYSURF.blit(Text(40).render("P1", True, (174, 137, 218)), (45, 450))        
+    DISPLAYSURF.blit(Text(40).render("P2", True, (124, 227, 228)), (86, 450))
+    for i in range(0,44,42):
+        pygame.draw.rect(DISPLAYSURF, Colours["BLACK"], (40 + i, 484, 40, 128), 4)
+        for j in range(0,5):
+            try:
+                if P1Wines[j] != (i == 42):
+                    DISPLAYSURF.blit(Text(32).render("W", True, Colours["BLACK"]), (48 + i , 488 + j  * 24))
+                else:
+                    DISPLAYSURF.blit(Text(32).render("L", True, Colours["BLACK"]), (52 + i, 488 + j * 24))
+            except IndexError:
+                pass
+    DISPLAYSURF.blit(Text(40).render(str(P1Score), True, Colours["BLACK"]), (50, 616))
+    DISPLAYSURF.blit(Text(40).render(str(P2Score), True, Colours["BLACK"]), (95, 616))
+    if COUNTDOWN != 5*60:
+        for i in range(150, 0, -10):
+            DISPLAYSURF.blit(Text(250).render(str(COUNTDOWN // FPS + 1), True, ((51*i)/50, (17*i)/15, (181*i)/150)), (SCREEN_WIDTH //2  - 50 + i/10, SCREEN_HEIGHT // 2 - 10 + i/10 ))
+
 #Setting up the pygame window
 pygame.init()
 
@@ -93,30 +133,23 @@ DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Game")
 
 P1, P2, S1, S2, level, P1text, P2text = setup()
-COUNTDOWN = FPS * 5 *0 +1
+COUNTDOWN = FPS * 5 
 
 P1ready = P2ready = False
 P1Wines = []
 P1Score = P2Score = 0
 winner = "NEW GAME"
 
+background = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+if not  Halloween:
+    background.fill(Colours["GREY"])
+for i in range(1,random.randint(1,20)):
+    pygame.draw.circle(background, (255, 0, 0, 200), (random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT)), random.randint(25, 100))
 #background = pygame.image.load(r'Images/Background.png')
 
 def Text(size):
     return pygame.font.SysFont('didot.ttc', size)
 
-for i in range(0, 255 * 3, 3):
-    for j in range(0, 255 * 3, 3):
-        DISPLAYSURF.set_at((i,j),  (i/3 % 255, j/3 % 255, (i/3)*(j/3) % 255))
-        DISPLAYSURF.set_at((i+1,j),  (i/3 % 255, j/3 % 255, (i/3)*(j/3) % 255))
-        DISPLAYSURF.set_at((i,j+1),  (i/3 % 255, j/3 % 255, (i/3)*(j/3) % 255))
-        DISPLAYSURF.set_at((i+1,j+1),  (i/3 % 255, j/3 % 255, (i/3)*(j/3) % 255))
-Col = pygame.Surface((255,255))
-for i in range(255):
-    for j in range(255):
-        Col.set_at((i,j),  (math.sin(i^j)*math.sin(j^i) % 255, math.cos(j^i)*math.cos(i^j) % 255, math.tan(i^j) * math.tan(j^i) % 255))
-        #pygame.draw.rect(DISPLAYSURF, (i % 255, j % 255, i*j % 255), (i ,j, 3 * i,3 * j))
-pygame.image.save(Col, "Colours.png")
 
 pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -126,8 +159,8 @@ while True:
     if game == "start":
 
         pygame.display.update()
-        #DISPLAYSURF.fill(Colours["GREY"])
-        #DISPLAYSURF.blit(background, (0,0))
+        DISPLAYSURF.fill(Colours["GREY"])
+        DISPLAYSURF.blit(background, (0,0))
 
 
         if pygame.key.get_pressed()[pygame.K_q]:
@@ -143,31 +176,7 @@ while True:
                  DISPLAYSURF.blit(Text(250).render(str(COUNTDOWN // FPS + 1), True, ((51*i)/50, (17*i)/15, (181*i)/150)), (SCREEN_WIDTH //2  - 50 + i/10, SCREEN_HEIGHT // 2 - 10 + i/10 ))
 
 
-        DISPLAYSURF.blit(Text(112).render(P1text, True, Colours["SHADOW"]), (43, 123))
-        DISPLAYSURF.blit(Text(112).render(P2text, True, Colours["SHADOW"]), (43, 203))
-        DISPLAYSURF.blit(Text(112).render(winner, True, Colours["SHADOW"]), (43, 43))
-        DISPLAYSURF.blit(Text(112).render(P1text, True, Colours["BLACK"]), (40, 120))
-        DISPLAYSURF.blit(Text(112).render(P2text, True, Colours["BLACK"]), (40, 200))
-        DISPLAYSURF.blit(Text(112).render(winner, True, Colours["BLACK"]), (40, 40))
-
-        DISPLAYSURF.blit(Text(40).render("Scores:", True, Colours["SHADOW"]), (42, 414)) 
-        DISPLAYSURF.blit(Text(40).render("Scores:", True, Colours["BLACK"]), (40, 412)) 
-        DISPLAYSURF.blit(Text(40).render("P1", True, Colours["SHADOW"]), (47, 452))       
-        DISPLAYSURF.blit(Text(40).render("P2", True, Colours["SHADOW"]), (88, 452))
-        DISPLAYSURF.blit(Text(40).render("P1", True, (174, 137, 218)), (45, 450))        
-        DISPLAYSURF.blit(Text(40).render("P2", True, (124, 227, 228)), (86, 450))
-        for i in range(0,44,42):
-            pygame.draw.rect(DISPLAYSURF, Colours["BLACK"], (40 + i, 484, 40, 128), 4)
-            for j in range(0,5):
-                try:
-                    if P1Wines[j] != (i == 42):
-                        DISPLAYSURF.blit(Text(32).render("W", True, Colours["BLACK"]), (48 + i , 488 + j  * 24))
-                    else:
-                            DISPLAYSURF.blit(Text(32).render("L", True, Colours["BLACK"]), (52 + i, 488 + j * 24))
-                except IndexError:
-                    pass
-        DISPLAYSURF.blit(Text(40).render(str(P1Score), True, Colours["BLACK"]), (50, 616))
-        DISPLAYSURF.blit(Text(40).render(str(P2Score), True, Colours["BLACK"]), (95, 616))
+        draw_menu(P1text, P2text, winner, P1Score, P2Score, COUNTDOWN)
 
 
         if COUNTDOWN == 0: 
@@ -206,11 +215,11 @@ while True:
         if P1_winner:
             winner = "LAST WINNER = P1"
             P1Score += 1
-            P1Wines.append(True)
+            P1Wines.insert(0, True)
         else:
             winner ="LAST WINNER = P2"
             P2Score += 1
-            P1Wines.append(False)
+            P1Wines.insert(0, False)
 
         P1, P2, S1, S2, level, P1text, P2text = setup()
         P1ready = P2ready = False
